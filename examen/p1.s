522c8090cada293c86cc6c189a24dcf2a0339fbd
Alexander <alrus2797@gmail.com>
2019-04-17 15:24:24 +0000

    examen 2

--- examen/p1.s
+++ examen/p1.s
@@ -1,19 +1,38 @@
 .data
-
+input1: .asciiz "Ingrese primer numero: "
+input2: .asciiz "Ingrese segundo numero: "
+output: .asciiz "El mayor es: "
 endl: .asciiz "\n"
 
 .text
 main:
+    li $v0, 4
+    la $a0, input1
+    syscall
+
     li $v0, 5
     syscall
 
     move $t1,$v0
+
+    li $v0, 4
+    la $a0, input2
+    syscall
+
+
     li $v0, 5
     syscall
+
     move $t2,$v0
 
     bge $t1, $t2, mayor
     
+    li $v0, 4
+    la $a0, output
+    syscall
+
+
+
     move $a0, $t2
     li $v0, 1
     syscall
